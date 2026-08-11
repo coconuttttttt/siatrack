4c421c31b6d5a8615dc618e5f88d530ce447ed33
Codecommanders <gambitojoshua0@gmail.com>
2026-08-11 23:19:51 +0800

    Allow dynamic MySQL password for both local setups

--- db_connect.php
+++ db_connect.php
@@ -1,12 +1,23 @@
 <?php
 $host = "localhost";
 $user = "root";
-$pass = "";
 $dbname = "siatrack_db";
 
-$conn = new mysqli($host, $user, $pass, $dbname);
+$passwords_to_try = ["", "root", "admin", "1234"]; 
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$conn = null;
+
+foreach ($passwords_to_try as $pwd) {
+    try {
+        $conn = @new mysqli($host, $user, $pwd, $dbname);
+        if (!$conn->connect_error) {
+            break; 
+        }
+    } catch (Exception $e) {
+    }
+}
+
+if (!$conn || $conn->connect_error) {
+    die("Connection failed: " . ($conn ? $conn->connect_error : "Could not connect to MySQL server. Please check your MySQL password."));
 }
 ?>
